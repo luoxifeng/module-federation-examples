aaebae2262f9de3f0726b71005bd7a6171981ee7
guoying13 <942394682@qq.com>
2021-08-16 16:25:01 +0800

    update

--- basic-host-remote/app2/src/Button.js
+++ basic-host-remote/app2/src/Button.js
@@ -3,3 +3,8 @@ import React from "react";
 const Button = () => <button>App 2 Button</button>;
 
 export default Button;
+
+window.app2___webpack_require__ = __webpack_require__
+
+console.dir(window.app2___webpack_require__, 'app2')
+
