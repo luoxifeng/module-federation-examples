690d0aa0f48f749b608eb12998be4ea7a818436d
luoxifeng <942394682@qq.com>
2021-08-25 19:33:27 +0800

    createData

--- basic-host-remote/app2/webpack.config.js
+++ basic-host-remote/app2/webpack.config.js
@@ -43,11 +43,11 @@ module.exports = {
       shared: { 
         react: { 
           singleton: true, 
-          eager: true 
+          // eager: true 
         },
         "react-dom": { 
           singleton: true, 
-          eager: true 
+          // eager: true 
         } 
       },
     }),
